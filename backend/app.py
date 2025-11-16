a8336926013f44460c66dc0db7d192341c6cdaca
varun123-lab <varunbarmavat@gmail.com>
2025-11-15 18:20:52 -0800

    basic project

---
+++ backend/app.py
@@ -0,0 +1,13 @@
+
+from flask import Flask
+from flask_cors import CORS
+
+app = Flask(__name__)
+CORS(app)
+
+@app.route("/")
+def home():
+    return {"message": "Theos AI Backend Running!"}
+
+if __name__ == "__main__":
+    app.run(debug=True)
